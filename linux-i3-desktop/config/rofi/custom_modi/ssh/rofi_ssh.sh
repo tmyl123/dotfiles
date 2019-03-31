78a5666f1afed1166a8aedb32a5c248709309eba
tommy <tommy@email.com>
2019-04-01 07:34:39 +0800

    fixed polybar

--- linux-i3-desktop/config/rofi/custom_modi/ssh/rofi_ssh.sh
+++ linux-i3-desktop/config/rofi/custom_modi/ssh/rofi_ssh.sh
@@ -12,6 +12,5 @@ if [ "$@" ]; then
     "$CWD"/ssh_wrapper.sh "$useraddress" > /dev/null
   fi
   else
-    echo "$editStr"
-    jq -r '.[] | .hostname + " => " + .user + "@" + .address' "$CWD"/hosts.json
+    jq -r '.[] | .hostname + " => " + .user + "@" + .address' "$CWD"/hosts.json | column -t
 fi
