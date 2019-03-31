720715f3d4082f354398dc089e769d96cffcd5f3
tommy <tommy@mail.com>
2019-03-31 22:22:21 +0800

    add ranger

---
+++ linux-i3-hidpi/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+[ -d ~/.config ] || mkdir ~/.config
+
+configfolders=( "custom_scripts" "dunst" "i3" "polybar" "rofi" "ranger")
+configfiles=( "zshrc" "tmux.conf" "Xresources" )
+
+for cf in "${configfolders[@]}"
+do
+  cp -r "$cf" ~/.config/"$cf"
+done
+
+for cf in "${configfiles[@]}"
+do
+  cp $cf ~/".$cf" 
+done
+
+cat fonts | xargs yay -S
