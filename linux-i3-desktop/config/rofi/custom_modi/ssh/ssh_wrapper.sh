af90c823501dc7f580dbc09b7f34f462883f61e1
tommy <tommy@mail.com>
2019-04-02 15:46:34 +0800

    add port to sshpass

--- linux-i3-desktop/config/rofi/custom_modi/ssh/ssh_wrapper.sh
+++ linux-i3-desktop/config/rofi/custom_modi/ssh/ssh_wrapper.sh
@@ -6,7 +6,10 @@ address=$(awk -F '@' '{print $2}' <<< "$@")
 pass=$(jq --arg address "$address" --arg user "$user"  '
        .[] 
        | select(.user == $user and .address == $address)' "$CWD"/hosts.json | jq -r .pass)
+port=$(jq --arg address "$address" --arg user "$user"  '
+       .[] 
+       | select(.user == $user and .address == $address)' "$CWD"/hosts.json | jq -r .port)
 #echo $user $address $pass > /tmp/foo
 
 #sshpass -p $pass ssh $user@$address
-urxvt -e sh -c "sshpass -p $pass ssh $user@$address" &
+urxvt -e sh -c "sshpass -p $pass ssh $user@$address -p $port" &
